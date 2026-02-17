8ac677df6a605256d84dc2b2e9b2414404c66ae7
Chiraag017 <chiraagm07@gmail.com>
2026-02-17 11:20:47 +0530

    UC9: Recursive Palindrome Checker

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -2,62 +2,37 @@ import java.util.Scanner;
 
 /**
  * Main class - PalindromeCheckerApp
- * Description: Checks for palindromes using a Singly Linked List and middle reversal.
+ * Description: Checks for palindromes using Recursion and the Call Stack.
  * @author Developer
- * @version 1.6
+ * @version 1.7
  */
-class Node {
-    char data;
-    Node next;
-    Node(char data) { this.data = data; }
-}
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your input: ");
         String input = sc.nextLine();
-        if (input.isEmpty()) return;
 
-        Node head = new Node(input.charAt(0));
-        Node current = head;
-        for (int i = 1; i < input.length(); i++) {
-            current.next = new Node(input.charAt(i));
-            current = current.next;
+        boolean isPalindrome = checkRecursive(input.toLowerCase(), 0, input.length() - 1);
+
+        if (isPalindrome) {
+            System.out.println("\"" + input + "\" True");
+        } else {
+            System.out.println("\"" + input + "\" false");
         }
 
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverseList(slow);
-        Node firstHalf = head;
-        Node secondHalfCopy = secondHalf;
-
-        boolean isPalindrome = true;
-        while (secondHalf != null) {
-            if (Character.toLowerCase(firstHalf.data) != Character.toLowerCase(secondHalf.data)) {
-                isPalindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        System.out.println("\"" + input + "\" " + isPalindrome);
         sc.close();
     }
 
-    private static Node reverseList(Node head) {
-        Node prev = null, current = head, next = null;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+    private static boolean checkRecursive(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
         }
-        return prev;
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false; // Mismatch found
+        }
+
+        return checkRecursive(str, start + 1, end - 1);
     }
 }
