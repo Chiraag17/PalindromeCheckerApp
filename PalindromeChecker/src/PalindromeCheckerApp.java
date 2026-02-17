f7842c04093a2569d5bbf15d33c8da17456126f9
Chiraag017 <chiraagm07@gmail.com>
2026-02-17 11:12:45 +0530

    UC6: Queue + Stack Based Palindrome Check

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,31 +1,40 @@
-import java.util.Scanner; // Required import for user input
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
+/**
+ * Main class - PalindromeCheckerApp
+ * Description: Checks for palindromes by comparing FIFO (Queue) vs LIFO (Stack).
+ * @author Developer
+ * @version 1.4
+ */
 public class PalindromeCheckerApp {
-    /**
-     * Main class - PalindromeCheckerApp
-     * Description:
-     * This class represents the entry point of the
-     * Palindrome Checker Management system.
-     * @author Developer
-     * @version 1.1
-     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your input: ");
-        String input = sc.nextLine();
+        S
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            stack.push(c);
+            queue.add(c);
+        }
 
         boolean isPalindrome = true;
-        int len = input.length();
 
+        while (!queue.isEmpty()) {
 
-        for (int i = 0; i < len / 2; i++) {
+            char fromQueue = Character.toLowerCase(queue.remove());
+            char fromStack = Character.toLowerCase(stack.pop());
 
-            if (input.charAt(i) != input.charAt(len - 1 - i)) {
+            if (fromQueue != fromStack) {
                 isPalindrome = false;
                 break;
             }
         }
-
 
         if (isPalindrome) {
             System.out.println("\"" + input + "\" True");
