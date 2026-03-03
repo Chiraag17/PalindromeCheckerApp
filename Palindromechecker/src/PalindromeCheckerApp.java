dc1a0a3f82c215cf93ada16eff05a9f5c5416be1
Chiraag017 <chiraagm07@gmail.com>
2026-03-03 09:06:07 +0530

    UC11: Object-Oriented Palindrome Service

---
+++ Palindromechecker/src/PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+
+public class PalindromeCheckerApp {
+
+    /**
+     * Checks whether the input string is a palindrome.
+     *
+     * @param input Input string
+     * @return true if palindrome, false otherwise
+     */
+    public boolean checkPalindrome(String input) {
+        String normalized = input.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+        if (normalized.isEmpty()) {
+            return false;
+        }
+
+        int left = 0;
+        int right = normalized.length() - 1;
+
+        while (left < right) {
+            if (normalized.charAt(left) != normalized.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
